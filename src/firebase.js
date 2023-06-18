5d51b601ab2c8ab13e79207ee7d87812d2298699
SergioBrownTrigoso <sergiobrown2003@gmail.com>
2023-06-17 21:45:31 -0500

    Configuración del archivo Registro.vue

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBoliQRQgrnOJzEFR04M6LrbcVvF-Y56kM",
+  authDomain: "aplicacion-web-fda76.firebaseapp.com",
+  projectId: "aplicacion-web-fda76",
+  storageBucket: "aplicacion-web-fda76.appspot.com",
+  messagingSenderId: "170467054122",
+  appId: "1:170467054122:web:3e4bb1215e8745f44ae953"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
